d074e867b9c5bc21712ce464c09a980f665a7faf
HalfBanana Dev <71050919+halfbananadev@users.noreply.github.com>
2023-08-11 04:33:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ tg.expand();
 
 let btn1 = document.getElementById("btn1")
 
-btn1.addListener("click", function(){
-  
-});
+//btn1.addListener("click", function(){
+//
+//});
 
 //Telegram.WebApp.onEvent("")
 tg.sendData("qqq");
 
 let p = document.createElement("p");
 
-p.innerText = `${tg.initDataUnsafe.user.first_name}`
+p.innerText = `${tg.initDataUnsafe.user.first_name}`;
