6b1c65fb71e30a49d38860ee1e299c1ef93bc2e0
HalfBanana Dev <71050919+halfbananadev@users.noreply.github.com>
2023-08-11 04:40:04 +0700

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,11 @@ let btn1 = document.getElementById("btn1")
 //Telegram.WebApp.onEvent("")
 tg.sendData("qqq");
 
+let usercard = document.getElementById("usercard");
+
 let p = document.createElement("p");
 
 p.innerText = `${tg.initDataUnsafe.user.first_name}
 ${tg.initDataUnsafe.user.last_name}`;
+
+usercard.appendChild(p);
